f4eea366d7e837d658d449653a8d8714349b69e4
pvn-w <pvn@pvn.pvn>
2019-11-01 09:05:23 -0400

    quicksort in-place

--- ex1/quicksort.js
+++ ex1/quicksort.js
@@ -7,13 +7,12 @@ const main = () => {
 	console.log(`Sorted array: ${res}`);
 
 	console.log(`Input array: ${arr}`);
-	res = quickSort(arr, rearrangeInplace);
-	console.log(`Sorted in=place array: ${res}`);
-	console.log(`Input array: ${arr}`);
+	quickSort(arr, rearrangeInplace);
+	console.log(`Sorted in-place array: ${arr}`);
 }
 
 const quickSort = (arr, method) => {
-	return method(arr, arr.length-1, 0, arr.length -1);
+	return method(arr, 0, arr.length -1);
 }
 
 const rearrange = arr => {
@@ -39,14 +38,17 @@ const rearrange = arr => {
 
 const rearrangeInplace = (arr, start, end) => {
 
-	console.log(`start=${start}; end=${end}; arr[${start}]=${arr[start]}; arr[${end}]=${arr[end]}`);
+	// console.log(`start=${start}; end=${end}; arr[${start}]=${arr[start]}; arr[${end}]=${arr[end]}`);
 
 	if(start >= end) {
-		return arr;
+		return;
 	}
 	let idx = partition(arr, start, end);
+	// let idx = res.idx;
+	// arr = res.arr;
 	rearrangeInplace(arr, 0, idx-1);
 	rearrangeInplace(arr, idx + 1, end);
+	// return arr;
 }
 
 
@@ -55,18 +57,25 @@ const partition = (arr, start, end) => {
 	let pIndex = start;
 	for(let i=start; i<end; i++) {
 		if(arr[i] <= pivot) {
-			arr = swap(arr, i, pIndex);
+			// console.log(`+++> arr[${i}]=${arr[i]}; pivot=${pivot}`);
+			swap(arr, i, pIndex);
 			pIndex++;
 		}
 	}
+	// console.log(`+++---+++> arr before=${arr}`);
+	swap(arr, end, pIndex);
+	// console.log(`+++---+++> arr after=${arr}`);
+	// return {arr: arr, idx: pIndex};
+	return pIndex;
 }
 
 const swap = (arr, idx1, idx2) => {
-	console.log(`~~~~~> arr=${arr}; idx1=${idx1}; idx2=${idx2}`);
+	// console.log(`~~~~~> Before swap arr=${arr}; idx1=${idx1}; idx2=${idx2}`);
 	const temp = arr[idx1];
 	arr[idx1] = arr[idx2];
 	arr[idx2] = temp;
-	return arr;
+	// console.log(`~~~~~> after swap arr=${arr}; idx1=${idx1}; idx2=${idx2}`);
+	// return arr;
 }
 
 main();
