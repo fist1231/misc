302422c8759b28f08d0b95ea171bc6b1547329a1
tsa <stislin@gmail.com>
2019-11-13 22:51:08 -0500

    BST hom done

--- ex1/bst.js
+++ ex1/bst.js
@@ -1,22 +1,136 @@
+import TreePeat from './treepeat';
 console.log('bst');
 
-const main = () => {
+class Bst {
 
-	// const arr = [24, 15, 2, -5, 34, 12, 228, 54, -6, -87, 20, 99, 3, -67, -43, 22];
-	const arr = [22, 60, 11, 55, 8, -2, 12, -6, 35, 33, 44, 39, 1, -3];
-	// const arr = [1,2,3,4,5,6,7,8,9,10];
-	// const arr = [10,8,9,6,7,4,5,2,3,1];
-	console.log(`arr: ${arr}`);
-	arr.sort((x,y)=>x-y);
-	// console.log(`arr: ${arr.sort()}`);
-	// console.log(`arr: ${arr.sort((x,y)=>x-y)}`);
-	console.log(`arr sorted: ${arr}`);
+	main() {
 
-	const bst = createBst(arr);
-	console.log(`BST: ${JSON.stringify(bst)}`);
+		// const arr = [24, 15, 2, -5, 34, 12, 228, 54, -6, -87, 20, 99, 3, -67, -43, 22];
+		const arr = [22, 60, 11, 55, 8, -2, 12, -6, 35, 33, 44, 39, 1, -3];
+		// const arr = [1,2,3,4,5,6,7,8,9,10];
+		// const arr = [10,8,9,6,7,4,5,2,3,1];
+		console.log(`arr: ${arr}`);
+		arr.sort((x,y)=>x-y);
+		// console.log(`arr sorted: ${arr}`);
+
+		const bst = this.createBst(arr);
+		console.log(`BST: ${JSON.stringify(bst)}`);
+
+		// const newBst = addElement(0, bst);
+
+		return bst;
+	}
+
+	rebalanceTree(bst) {
+		// const bst = this.main();
+		const newBst = this.addElement(0, bst);
+		const rebBst = this.rebalance(newBst);
+		return rebBst;
+	}
+
+	rebalance(bst) {
+		console.log(`rrrrrrr> bst before rebalance: ${JSON.stringify(bst)}`);
+		const arr = Tree.toArray(bst);
+		arr.sort((x,y)=>x-y);
+		console.log(`rrrrrrr> arr: ${arr}`);
+		const reb = this.createBst(arr);
+		console.log(`rrrrrrr> bst after  rebalance: ${JSON.stringify(reb)}`);
+		return reb;
+	}
+
+	addElement(x, bst) {
+
+		console.log(`^^^^^ bst before: ${JSON.stringify(bst)}`);
+		this.findWhere(x, bst.top);
+		console.log(`^^^^^ bst  after: ${JSON.stringify(bst)}`);
+
+		return bst;
+	}
+
+	findWhere(x, node) {
+
+		if(x === node.id) {
+			console.log(`xxxxx> Node with ${x} already exists in the tree`);
+			return undefined;
+		}
+		if(x > node.id) {
+			if(node.right) {
+				this.findWhere(x, node.right);
+			} else {
+				//insert here
+				console.log(`Insert on the right of ${node.id}`);
+				node.right = new Node(x);
+			}
+		}
+		if(x < node.id) {
+			if(node.left) {
+				this.findWhere(x, node.left);
+			} else {
+				// insert here
+				console.log(`Insert on the left of ${node.id}`);
+				node.left = new Node(x);
+			}
+		}
+	}
+
+
+
+	createBst(arr) {
+		const node = this.addNode(arr);
+		const tree = new Tree();
+		tree.top = node; 
+		return tree;
+	}
+
+	addNode(arr, node, side) {
+		if(arr.length < 1) {
+			return undefined;
+		}
+		const mid = Math.floor(arr.length / 2);
+		if(node && side) {
+			node[side] = new Node(arr[mid]);
+		} else {
+			node = new Node(arr[mid]); 
+		}
+		this.addNode(arr.slice(0, mid), side?node[side]:node, 'left');
+
+		if(arr.length > 2) {
+			this.addNode(arr.slice(mid+1, arr.length), side?node[side]:node, 'right');
+		}
+		return node;
+	}
 }
 
-const createBst = (arr) => {
+
+class Tree {
+	constructor() {
+		this.top = undefined;
+	}
+
+	static toArray(tree) {
+		const tp = new TreePeat();
+		return tp.traverseDepth(tree); 
+	}
+}
+
+class Node {
+	constructor(id) {
+		this.id = id;
+		this.left = undefined;
+		this.right = undefined;
+	}
+}
+
+ // main();
+// rebalanceTree();
+
+export default Bst;
+
+
+
+ //================= First atttempt =====================================
+ /*
+ const createBst = (arr) => {
 	
 	const tree = new Tree();
 
@@ -42,21 +156,7 @@ const createBst = (arr) => {
 
 	tree.top = node; 
 
-
-	// let arrLeft = arr.slice(0, mid);
-	// console.log(`arrLeft: ${arrLeft}`);
-	// let lmid = Math.floor((arrLeft.length - 1) / 2) + 1;
-	// node.left = new Node(arrLeft[lmid]);
-	// console.log(`leftMid: ${lmid}; arrLeft[mid] = ${arrLeft[lmid]}`);
-
-	// let arrRight = arr.slice(mid+1, arr.length);
-	// console.log(`arrRight: ${arrRight}`);
-	// let rmid = Math.floor((arrRight.length - 1) / 2) + 1;
-	// node.right = new Node(arrRight[rmid]);
-	// console.log(`rightMid: ${rmid}; arr[mrightMid] = ${arrRight[rmid]}`);
-
 	return tree;
-
 }
 
 const addLeft = (arr, node) => {
@@ -122,20 +222,4 @@ const addRight = (arr, node) => {
 
 	return node;
 }
-
-
-class Tree {
-	constructor() {
-		this.top = undefined;
-	}
-}
-
-class Node {
-	constructor(id) {
-		this.id = id;
-		this.left = undefined;
-		this.right = undefined;
-	}
-}
-
- main();
+*/
